3e3940d1ca740eee2c1ccf0686303cbf6d6d2708
SoVa <od.thanatos@gmail.com>
2021-09-25 10:09:50 +0300

    craft-item refactoring, mining hardening

--- Crafting/craft_item.py
+++ Crafting/craft_item.py
@@ -1,44 +1,80 @@
 from py_stealth.methods import *
+import inspect
 
+TARGET = "dagger"
+KEEP = False
+VERBOSE = True
+#
 TINKER_TOOLS = 0x1EB8
 INGOTS = 0x1BF2
-LOCKPICK = 0x14FC
+CRAFTING_GUMP = 0x38920ABD
+ITEMS = {
+    "tinker_tools": {"category": 8, "button": 23, "type": 0x1EB8, "tool": "tinker_tools", "material": 0x1BF2},
+    "saw": {"category": 8, "button": 65, "type": 0x1034, "tool": "tinker_tools", "material": 0x1BF2},
+    "lockpick": {"category": 8, "button": 149, "type": 0x14FC, "tool": "tinker_tools", "material": 0x1BF2},
+    "smiths_hammer": {"category": 8, "button": 121, "type": 0x0FB4, "tool": "tinker_tools", "material": 0x1BF2},
+    "dagger": {"category": 43, "button": 44, "type": 0x0F52, "tool": "smiths_hammer", "material": 0x1BF2},
 
-def craft_item(tool_category: int, tool_button: int, tool_type: int, item_type: int, required_qty: int) -> None:
-    while Count(item_type) < required_qty:
-        for _gump_counter in range(0, GetGumpsCount()):
-            CloseSimpleGump(_gump_counter)
+}
 
-        if FindType(tool_type, Backpack()):
-            UseType(tool_type, 0x0000)
-            Wait(500)
-            wait_for_gump(tool_category)
-            wait_for_gump(tool_button)
-            wait_for_gump(0)
+def log(message = ""):
+    if VERBOSE:
+        AddToSystemJournal(f"[{inspect.stack()[1].function}] {message}")
+
+def craft_item(item: dict, required_qty: int = 9999) -> None:
+    if Count(item["type"]) < required_qty:
+        for gump_counter in range(0, GetGumpsCount()):
+            CloseSimpleGump(gump_counter)
+
+        tool = ITEMS[item["tool"]]["type"]
+        if FindType(tool, Backpack()):
+            UseType(tool, 0xFFFF)
+            for button in [item["category"], item["button"], 0]:
+                wait_for_gump(button)
+        else:
+            log("No tools in backpack!")
 
 
 def find_gump(gump_id: int) -> bool:
-    for _gump in range(0, GetGumpsCount()):
-        if GetGumpID(_gump) == gump_id:
+    for gump in range(0, GetGumpsCount()):
+        if GetGumpID(gump) == gump_id:
             return True
     return False
 
 
 def wait_for_gump(button: int) -> None:
-    _try = 0
-    while not find_gump(0x38920ABD):
-        _try += 1
-        Wait(500)
-        if _try > 30:
-            # print("wat_for_gump timeout")
+    attempt = 0
+    while not find_gump(CRAFTING_GUMP):
+        attempt += 1
+        Wait(100)
+        if attempt > 30:
+            log("wat_for_gump timeout")
             return
 
     WaitGump(button)
     Wait(2000)
 
 if __name__ == "__main__":
-    print("Ok")
-    while FindTypeEx(INGOTS, 0x0000, Backpack()) and FindQuantity() > 10:
-        print("Crafting")
-        craft_item(8, 23, TINKER_TOOLS, TINKER_TOOLS, 2)
-        craft_item(8, 149, TINKER_TOOLS, LOCKPICK, 2000)
+    item_to_craft = ITEMS[TARGET]
+    required_tools = ITEMS[item_to_craft["tool"]]
+    tinker_tools = ITEMS["tinker_tools"]
+
+    while not Dead():
+        # Keep 2 tinker tools, required tools in pack
+        # Craft 1 target item to be able to use Make Last button
+        craft_item(tinker_tools, 2)
+        craft_item(required_tools, 2)
+        craft_item(item_to_craft)
+
+        UseType(required_tools["type"], 0xFFFF)
+        wait_for_gump(21)
+        for _ in range(10):
+            # If we can reuse opened gump
+            if find_gump(CRAFTING_GUMP):
+                # Make last
+                log("Making last item...")
+                wait_for_gump(21)
+
+        if not KEEP:
+            AutoSell(item_to_craft["type"], 0xFFFF, -1)
+            UOSay("sell")
--- Crafting/mining.py
+++ Crafting/mining.py
@@ -1,24 +1,28 @@
 from py_stealth.methods import *
 from datetime import datetime as dt
 import inspect
-##########
-SHOVEL = 0x0F39
+# Can be changed
+TILE_SEARCH_RANGE = 15
+# INITIAL_LOCATION = (3037, 3972)
+# FORGE_LOCATION = (3037, 3972)
+#FORGE = 0x198E
+
+INITIAL_LOCATION = (3130, 3866)
+FORGE_LOCATION = (3130, 3865)
+FORGE = 0x197E
+VERBOSE = True
+# Generic types
+SHOVEL = 0x0F3A
+MINING_BAG = 0x1C10
 TINKER_TOOLS = 0x1EB8
-ORE = [0x19B9, 0x19B8, 0x19B7]
-SMALL_ORE = 0x19B7
-#FORGE = 0x0FB1
-FORGE = 0x198E
-## Locations
-INITIAL_LOCATION = (3039, 3958)
-FORGE_LOCATION = (3037, 3972)
-##
-####
+ORE = 0x19B9
+
+
+# Generic messages and tiles
 SKIP_TILE_MESSAGES = ["no metal", "far away"]
 NEXT_TRY_MESSAGES = ["loosen", "You dig some", "You dig up"]
-
 CAVE_TILES = range(1339, 1358)
 
-VERBOSE = True
 ##########
 
 def log(message = ""):
@@ -40,19 +44,27 @@ def find_tiles(center_x: int, center_y: int, radius: int) -> set[int, int, int, 
     log(f"Found {str(len(_tiles_coordinates))} tiles")
     return _tiles_coordinates
 
+def to_bag() -> None:
+    if bag := FindType(MINING_BAG, Backpack()):
+        while FindType(ORE, Backpack()):
+            MoveItem(FindItem(), -1, bag, 0, 0, 0)
+            Wait(1000)
+
+
 def smelt():
     IgnoreReset()
     forge_x, forge_y = FORGE_LOCATION
     newMoveXY(forge_x, forge_y, True, 1, True)
 
-    # Stack small ore piles
-    # TODO: Color ore support
-    while FindType(SMALL_ORE, Backpack()) and len(GetFoundList()) > 1:
-        UseObject(GetFoundList()[0])
-        WaitTargetObject(GetFoundList()[1])
+    while LastContainer() != Backpack():
+        UseObject(Backpack())
         Wait(1000)
 
-    while FindTypesArrayEx(ORE, [0xFFFF], [Backpack()], False):
+    while LastContainer() == Backpack():
+        UseType(MINING_BAG, 0xFFFF)
+        Wait(1200)
+
+    while FindType(ORE, LastContainer()):
         # You can't smelt 1 ore =\
         if GetQuantity(FindItem()) > 1:
             WaitTargetTile(FORGE, forge_x, forge_y, GetZ(Self()))
@@ -88,7 +100,7 @@ def wait_for_gump(button: int) -> None:
         _try += 1
         Wait(500)
         if _try > 30:
-            # print("wat_for_gump timeout")
+            log("wat_for_gump timeout")
             return
 
     WaitGump(button)
@@ -100,15 +112,19 @@ def mine(tile):
     if newMoveXY(x, y, True, 1, True):
         while not Dead():
             # Smelt and return back to mining point
-            if Weight() > 330:
-                smelt()
-                newMoveXY(x, y, True, 1, True)
-                craft_item(8, 23, TINKER_TOOLS, TINKER_TOOLS, 2)
-                craft_item(8, 86, TINKER_TOOLS, SHOVEL, 2)
-
+            if Weight() > 220:
+                to_bag()
+                if Weight() > 250:
+                    smelt()
+                    newMoveXY(x, y, True, 1, True)
             started = dt.now()
             cancel_targets()
-            UseType(SHOVEL, 0xFFFF)
+            if FindType(SHOVEL, Backpack()):
+                UseType(SHOVEL, 0xFFFF)
+            else:
+                craft_item(8, 23, TINKER_TOOLS, TINKER_TOOLS, 2)
+                craft_item(8, 93, TINKER_TOOLS, SHOVEL, 2)
+
             WaitForTarget(2000)
             if TargetPresent():
                 WaitTargetTile(tile, x, y, z)
@@ -122,7 +138,7 @@ def mine(tile):
 if __name__ == "__main__":
     init_x, init_y = INITIAL_LOCATION
     newMoveXY(init_x, init_y, True, 1, True)
-    tiles = find_tiles(GetX(Self()), GetY(Self()), 20)
-    for tile in range(0, len(tiles), 4):
-        # print(tiles[tile])
-        mine(tiles[tile])
+    tiles = find_tiles(GetX(Self()), GetY(Self()), TILE_SEARCH_RANGE)
+    while not Dead() and Connected():
+        for tile in range(0, len(tiles), 4):
+            mine(tiles[tile])
